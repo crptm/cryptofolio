286c2edd7398cca775460f5b8913aa68dfb75735
crptm <crptm@users.noreply.github.com>
2017-04-16 16:49:30 +0400

    prettier + fresh data

--- src/App.js
+++ src/App.js
@@ -1,64 +1,78 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import TickerData from './mock.json';
+import TickerData from "./mock.json";
 
 function getValue(symbol, amount) {
-  const data = TickerData.find(x => x.symbol === symbol)
-  return (+data.price_usd * amount).toFixed(2)
+  const data = TickerData.find(x => x.symbol === symbol);
+  return (+data.price_usd * amount).toFixed(2);
 }
-
 
 function Symbols(props) {
   if (!props.symbols || !props.symbols.length) {
-    return <div>{'Empty'}</div>;
+    return <div>{"Empty"}</div>;
   }
-  return <div>{props.symbols.map(x => <SymbolRow key={x.symbol} symbol={x.symbol} amount={x.amount} />)}</div>;
-}
-
-
-function SymbolRow(props) {
   return (
-    <div style={{display: 'flex'}}>
-      <div style={{width: 50}}>{props.symbol}</div>
-      <div style={{width: 100, textAlign: 'right'}}>{props.amount}</div>
-      <div style={{width: 100, textAlign: 'right'}}>${getValue(props.symbol, props.amount)}</div>
+    <div>
+      {props.symbols.map(x => (
+        <SymbolRow key={x.symbol} symbol={x.symbol} amount={x.amount} />
+      ))}
     </div>
   );
 }
 
+function SymbolRow(props) {
+  return (
+    <div style={{ display: "flex" }}>
+      <div style={{ width: 50 }}>{props.symbol}</div>
+      <div style={{ width: 100, textAlign: "right" }}>{props.amount}</div>
+      <div style={{ width: 100, textAlign: "right" }}>
+        ${getValue(props.symbol, props.amount)}
+      </div>
+    </div>
+  );
+}
 
 class AddSymbol extends React.Component {
   state = {
-    symbol: '',
-    amount: ''
-  }
+    symbol: "",
+    amount: ""
+  };
   render() {
     return (
       <div>
-        <input name="symbol" value={this.state.symbol} placeholder="symbol" onChange={this.handleChange}/>
-        <input name="amount" value={this.state.amount}  placeholder="amount" onChange={this.handleChange}/>
+        <input
+          name="symbol"
+          value={this.state.symbol}
+          placeholder="symbol"
+          onChange={this.handleChange}
+        />
+        <input
+          name="amount"
+          value={this.state.amount}
+          placeholder="amount"
+          onChange={this.handleChange}
+        />
 
         <button onClick={this.handleAdd}>Add</button>
       </div>
     );
   }
 
-  handleChange = (e) => {
+  handleChange = e => {
     this.setState({
       [e.target.name]: e.target.value
     });
-  }
+  };
 
   handleAdd = () => {
     this.props.add(+this.state.amount, this.state.symbol);
     this.setState({
-      symbol: '',
-      amount: ''
-    })
-  }
+      symbol: "",
+      amount: ""
+    });
+  };
 }
-
 
 export default class App extends React.Component {
   render() {
@@ -67,6 +81,6 @@ export default class App extends React.Component {
         <Symbols symbols={this.props.state.symbols} />
         <AddSymbol add={this.props.addSymbol} />
       </div>
-    )
+    );
   }
 }
--- src/Container.js
+++ src/Container.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import localforage from 'localforage';
-import App from './App'
+import React, { Component } from "react";
+import localforage from "localforage";
+import App from "./App";
 
 export default class Container extends Component {
-  state = {}
+  state = {};
 
   async componentDidMount() {
     localforage.config();
-    const state = await localforage.getItem('state');
+    const state = await localforage.getItem("state");
     console.info("Got state", state);
-    this.setState({state: state || {}});
+    this.setState({ state: state || {} });
   }
 
   render() {
@@ -17,25 +17,23 @@ export default class Container extends Component {
       return null;
     }
 
-    return (
-      <App state={this.state.state} addSymbol={this.handleAddSymbol} />
-    );
+    return <App state={this.state.state} addSymbol={this.handleAddSymbol} />;
   }
 
   handleAddSymbol = (amount, symbol) => {
     const symbols = this.state.state.symbols || [];
     const index = symbols.findIndex(x => x.symbol === symbol);
     if (index === -1) {
-      symbols.push({symbol, amount});
+      symbols.push({ symbol, amount });
     } else {
-      const oldVal = symbols[index]
-      symbols[index] = {symbol, amount: amount + oldVal.amount}
+      const oldVal = symbols[index];
+      symbols[index] = { symbol, amount: amount + oldVal.amount };
     }
-    this.storeState({...this.state.state, symbols});
-  }
+    this.storeState({ ...this.state.state, symbols });
+  };
 
   async storeState(state) {
-    await localforage.setItem('state', {...state, version: 1});
-    this.setState({state});
+    await localforage.setItem("state", { ...state, version: 1 });
+    this.setState({ state });
   }
 }
